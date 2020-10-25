e23ae853d98ac13d185fbe2bc7b141a188a81198
Mohammad Rehan <mohdrehan.in@gmail.com>
2020-10-25 16:56:18 +0530

    commit2

--- Covid19Reports/src/test/java/com/covid/restapi/CovidTestController.java
+++ Covid19Reports/src/test/java/com/covid/restapi/CovidTestController.java
@@ -1,28 +1,66 @@
 package com.covid.restapi;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
 
-@SpringBootTest
-class CovidTestController extends Covid19ReportsApplicationTests {
+import com.covid.restapi.covid19dataserviceImpl.Covid19DataServiceImpl;
+import com.covid.restapi.entity.CovidReport;
+
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+
+class CovidTestController {
 
 	@Autowired
 	private WebApplicationContext webApplicationContext;
+	
+	@Autowired
+	RestTemplate rs;
+	
+	@Mock
+	Covid19DataServiceImpl covid19DataServiceImpl;
 	
 	private MockMvc mockMvc;
 	
 	@Test
 	public void covidTest() throws Exception {
-		mockMvc=MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-		mockMvc.perform(get("/retrieve")).andExpect(status().isOk()).andExpect(content().contentType("application/json"));
+		/*
+		 * System.out.println("Inside test");
+		 * mockMvc=MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+		 * mockMvc.perform(get("/retrieve")).andExpect(status().isOk()).andExpect(
+		 * content().contentType("application/json"));
+		 */
+		String url="https://coronavirus-19-api.herokuapp.com/countries"; 
 		
+		  
+		  
+		  ResponseEntity<List<CovidReport>> response = rs.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<List<CovidReport>>() {
+		});
+		  List<CovidReport> list = response.getBody();
+		  List<CovidReport> list2 = list.stream().limit(10).collect(Collectors.toList());
+		
+		Mockito.when( rs.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<List<CovidReport>>() {
+		})).thenReturn(new ResponseEntity<List<CovidReport>>(list, HttpStatus.OK));
+		ResponseEntity<List<CovidReport>> list3 = covid19DataServiceImpl.getCovid19Data();
+		assertEquals(list2, list3);
 	}
 }
